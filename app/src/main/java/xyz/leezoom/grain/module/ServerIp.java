a1a641de08fb5ceefa94cd4f81ca1c5e0187bfe8
Jet Lee <reindeerlee.work@gmail.com>
2017-10-21 18:40:15 +0800

    Deleted server ip and port

--- app/src/main/java/xyz/leezoom/grain/module/ServerIp.java
+++ app/src/main/java/xyz/leezoom/grain/module/ServerIp.java
@@ -10,17 +10,19 @@ package xyz.leezoom.grain.module;
 /**
  * @Author lee
  * @Time 9/3/17.
+ * Because of some reasons, I hava deleted server ip and port in this file.
+ * If you really want to know, you'll have a way by yourself.
  */
 public class ServerIp {
-    public final static String mainIp="61.153.216.108";
+    public final static String mainIp="127.0.0.1";
     //校园卡相关
-    public final static String cardServerPort="11088";
+    public final static String cardServerPort="";
     //学生,班级,成绩等信息相关
-    public final static String mainServerPort="15088";
+    public final static String mainServerPort="";
     //token,验证相关
-    public final static String baseServerPort="14088";
+    public final static String baseServerPort="";
     //图书馆相关
-    public final static String libraryServerPort="13088";
+    public final static String libraryServerPort="";
     //蓝色大讲堂相关
-    public final static String blueCourseServerPort="16088";
+    public final static String blueCourseServerPort="";
 }
